2eb64c212b40a277e395e2479e1f3730b37558e9
Joshua Avalon <git@avalon.sh>
2026-07-15 23:13:10 +0800

    feat: Add react rules

--- src/lint/index.ts
+++ src/lint/index.ts
@@ -1,5 +1,6 @@
 export { eslint } from "./eslint.js";
 export { importCfg } from "./import.js";
 export { oxc } from "./oxc.js";
+export { react } from "./react.js";
 export { typescript } from "./typescript.js";
 export { unicorn } from "./unicorn.js";
